27ab759c02ecbf1ea2f91c4a95674d5a67e825d0
Imanuel Costigan <i.costigan@me.com>
2016-08-20 20:20:41 +1000

    Update SQLServerResult class doc

--- R/dbi-classes.R
+++ R/dbi-classes.R
@@ -26,14 +26,21 @@ setClass("SQLServerConnection", contains = 'DBIConnection',
 
 #' SQL Server Result classes
 #'
-#' The \code{SQLServerResult} class extends the \code{DBIResult} class, while
-#' the \code{SQLServerUpdateResult} class extends the \code{SQLServerResult}
-#' class. The latter is created by a called to \code{dbSendStatement} as the
-#' JDBC API does not return a ResultSet but rather an integer value. The
-#' \code{dbGetRowsAffected} called on \code{SQLServerUpdateResult} returns the
-#' value produced by the JDBC API.
+#' The \code{SQLServerPreResult} class extends the \code{DBIResult} class, the
+#' \code{SQLServerResult} class extends the \code{SQLServerPreResult} class,
+#' while the \code{SQLServerUpdateResult} class extends the
+#' \code{SQLServerResult} class. The \code{SQLServerUpdateResult} class is
+#' created by a called to \code{dbSendStatement} as the JDBC API does not return
+#' a ResultSet but rather an integer value. The \code{dbGetRowsAffected} called
+#' on \code{SQLServerUpdateResult} returns the value produced by the JDBC API.
+#' The \code{SQLServerPreResult} class wraps a JDBC PreparedStatement that has
+#' yet to be executed and is necessary for \code{dbBind} to function per the DBI
+#' spec (taking a DBIResult as an input and returning another as output). This
+#' contrasts to the JDBC interface where binding occurs on Statement classes and
+#' not on ResultSet classes.
 #'
 #' @keywords internal
+#' @rdname SQLServerResult-class
 #' @export
 
 setClass ("SQLServerPreResult", contains = 'DBIResult',
